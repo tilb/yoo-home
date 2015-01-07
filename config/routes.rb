c3c9e5b7785b26c32510281f837cc11567c47019
tilb <ernestmistiaen@Ernests-MacBook-Pro.local>
2015-01-07 15:59:32 +0100

    Added email

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#roompage'
   get "pattern-library" => "pages#pattern-library"
+  get "email" => "pages#email"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
